99705a5a6a777ab33d007897600c0d60139545d4
Sahil Chiwhane <137439558+SahilChiwhane@users.noreply.github.com>
2023-09-24 17:21:12 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Age in Days Calculator
+
+function ageInDayss() {
+  var birthYear = prompt("What year you were Born?");
+  var ageInDayss = (2023 - birthYear) * 365;
+  var h1 = document.createElement('h1');
+  var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old.');
+  h1.setAttribute('id', 'ageInDays');
+  h1.appendChild(textAnswer);
+  document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset() {
+    document.getElementById('ageInDays').remove();                      
+}
